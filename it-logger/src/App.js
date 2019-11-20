234eff0be443317a238e95e68c29b0fb3d4f3c2f
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-11-20 01:43:19 -0500

    added modal for adding logs

--- it-logger/src/App.js
+++ it-logger/src/App.js
@@ -5,6 +5,8 @@ import Logs from "./components/logs/Logs";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
 
 const App = () => {
   useEffect(() => {
@@ -15,6 +17,8 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
