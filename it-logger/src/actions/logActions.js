f5fc40bd871f0a25d290931d8177a52c7b511c41
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-11-21 22:26:00 -0500

    can delete logs using redux

--- it-logger/src/actions/logActions.js
+++ it-logger/src/actions/logActions.js
@@ -1,4 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG
+} from "./types";
 
 /* one way of writing a function below */
 // export const getLogs = () => {
@@ -49,6 +55,24 @@ export const addLog = log => async dispatch => {
     dispatch({
       type: ADD_LOG,
       payload: data
+    });
+  } catch (err) {
+    dispatch({ type: LOGS_ERROR, payload: err.response.data });
+  }
+};
+
+// delete log from the server
+export const deleteLog = id => async dispatch => {
+  try {
+    setLoading();
+
+    await fetch(`/logs/${id}`, {
+      method: "DELETE"
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id
     });
   } catch (err) {
     dispatch({ type: LOGS_ERROR, payload: err.response.data });
