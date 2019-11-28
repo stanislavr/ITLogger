e267120f9420c5bdf31c1d9c9996a8883411e679
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-11-27 23:44:38 -0500

    can add techs using redux

--- it-logger/src/actions/techActions.js
+++ it-logger/src/actions/techActions.js
@@ -22,6 +22,30 @@ export const getTechs = () => async dispatch => {
     dispatch({ type: TECHS_ERROR, payload: err.statusText });
   }
 };
+
+// add technician to server
+export const addTech = tech => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch("/techs", {
+      method: "POST",
+      body: JSON.stringify(tech),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({ type: TECHS_ERROR, payload: err.statusText });
+  }
+};
+
 // set loading to true
 export const setLoading = () => {
   return {
--- it-logger/src/components/techs/AddTechModal.js
+++ it-logger/src/components/techs/AddTechModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addTech } from "../../actions/techActions";
 import M from "materialize-css/dist/js/materialize.min.js";
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
 
@@ -9,7 +12,9 @@ const AddTechModal = () => {
     if (firstName === "" || lastName === "") {
       M.toast({ html: "Please enter the first and last name of the tech" });
     } else {
-      console.log(firstName, lastName);
+      addTech({ firstName, lastName });
+
+      M.toast({ html: `${firstName} ${lastName} was added as a technician` });
 
       // clear fields
       setFirstName("");
@@ -63,4 +68,8 @@ const AddTechModal = () => {
   );
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+  addTech: PropTypes.func.isRequired
+};
+
+export default connect(null, { addTech })(AddTechModal);
